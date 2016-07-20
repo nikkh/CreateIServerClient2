ea65557fe8183223426e08145500b880c08a40db
nikkh <nikkh.noreply.github.com>
2016-07-20 12:44:41 +0100

    Initial Commit

--- CreateIServerClient2/Scripts/Deploy-AzureResourceGroup.ps1
+++ CreateIServerClient2/Scripts/Deploy-AzureResourceGroup.ps1
@@ -4,13 +4,13 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'CreateIServerClient2',
+    [string] $ResourceGroupName = 'CreateIServerClient',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageAccountResourceGroupName, 
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = '..\Templates\WindowsVirtualMachine.json',
-    [string] $TemplateParametersFile = '..\Templates\WindowsVirtualMachine.parameters.json',
+    [string] $TemplateFile = '..\Templates\AzureDeploy.json',
+    [string] $TemplateParametersFile = '..\Templates\AzureDeploy.parameters.json',
     [string] $ArtifactStagingDirectory = '..\bin\Debug\staging',
     [string] $AzCopyPath = '..\Tools\AzCopy.exe',
     [string] $DSCSourceFolder = '..\DSC'
